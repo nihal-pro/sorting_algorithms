f674aee8461f015b162e738b4e07e585572f8a5a
NIHAL <nihaleloudiki16@gmail.com>
2024-01-28 00:17:34 +0100

    handle task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int tmp;
+
+    if (array == NULL || size <= 1)
+        return;
+
+    for (i = 0; i < size-1; i++)
+    {
+        for (j = i+1; j < size; j++)
+        {
+            if (array[j] < array[i])
+            {
+                tmp = array[j];
+                array[j] = array[i];
+                array[i] = tmp;
+            }
+            print_array(array, size);
+        }
+    }
+}
