dc0329f39b72fe9708b1416b144ee647f8a8ea03
NIHAL <nihaleloudiki16@gmail.com>
2024-01-28 16:13:18 +0100

    handle tasks

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,21 +10,26 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t select, i, j;
-	int tmp;
+	int tmp, count;
 
 	if (array == NULL || size <= 1)
 		return;
 	for (i = 0; i < size; i++)
 	{
 		select = i;
+		count = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[select] > array[j])
+			{
 				select = j;
+				count++;
+			}
 		}
 		tmp = array[i];
 		array[i] = array[select];
 		array[select] = tmp;
-		print_array(array, size);
+		if (count)
+			print_array(array, size);
 	}
 }
