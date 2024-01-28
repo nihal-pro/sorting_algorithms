3168c6a8653e663ca11f0c271ac9e9c34cb57b91
NIHAL <nihaleloudiki16@gmail.com>
2024-01-28 15:42:34 +0100

    handle tasks

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * selection_sort - function that sorts an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t select, i, j;
+	int tmp;
+
+	if (array == NULL || size <= 1)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		select = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[select] > array[j])
+				select = j;
+		}
+		tmp = array[i];
+		array[i] = array[select];
+		array[select] = tmp;
+		print_array(array, size);
+	}
+}
