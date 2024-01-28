eda86dea38748551005c5e83fe10ab93d053551d
NIHAL <nihaleloudiki16@gmail.com>
2024-01-28 12:34:39 +0100

    handle tasks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,24 +9,24 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, repeat = size;
-    int tmp;
+	size_t i, repeat = size;
+	int tmp;
 
-    if (array == NULL || size <= 1)
-        return;
+	if (array == NULL || size <= 1)
+		return;
 
-    while (repeat >= 1)
-    {
-        for (i = 0; i < size-1; i++)
-        {
-            if (array[i] > array[i+1])
-            {
-                tmp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = tmp;
-                print_array(array, size);
-            }
-        }
-        repeat--;
-    }
+	while (repeat >= 1)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		repeat--;
+	}
 }
